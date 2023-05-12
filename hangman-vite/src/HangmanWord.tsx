80de5bc133dfa05c5f3a066fe475c4da2fce0dc6
Kobe <kobetbuckley@gmail.com>
2023-05-11 19:46:36 -1000

    finished the setup for the guessed letters showing up only if they match the answer's letters

--- hangman-vite/src/HangmanWord.tsx
+++ hangman-vite/src/HangmanWord.tsx
@@ -1,7 +1,8 @@
 export function HangmanWord() {
-
-    const word = "test"
-    return <div
+    const word = "test"    
+    const guessedLetters = ['t','e']
+    return (
+    <div
     style= {{
       display: "flex", 
       gap: ".25em", 
@@ -12,7 +13,13 @@ export function HangmanWord() {
     }}
     >
         {word.split("").map((letter, index) => (
-            <span>{letter}</span>
+            <span style={{ borderBottom: ".1em solid black" }} key={index}>
+                <span style={{
+                    visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+                }}>
+                    {letter}
+                    </span>
+                </span>
         ))}
-    </div>
+    </div>)
 }
