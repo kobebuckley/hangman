6f7c1893ac1a6ff9ef817ee3ec2cacd6da7868cb
Kobe <kobetbuckley@gmail.com>
2023-05-11 20:17:10 -1000

    added the keybaord base functionality

--- hangman-vite/src/Keyboard.tsx
+++ hangman-vite/src/Keyboard.tsx
@@ -1,3 +1,18 @@
+const KEYS = [
+
+]
+
 export function Keyboard(){
-    
+    return(
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: "repeat(auto-fit, minmax(75, 1fr)",
+            gap: ".5rem",
+          }}
+          >
+           {KEYS.map(key => {
+            return <button key={key}>{key}</button>
+           })}
+          </div>
+        )
 }
