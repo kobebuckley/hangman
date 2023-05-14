1115a65ca879321f1f7f6c2f5264f7b5db1c158f
Kobe <kobetbuckley@gmail.com>
2023-05-13 17:05:23 -1000

    basic hangmane functionality except visual keyboard is complete

--- hangman-vite/src/App.tsx
+++ hangman-vite/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import words from "./wordList.json"
 import { HangmanDrawing } from "./HangmanDrawing";
 import { HangmanWord } from "./HangmanWord";
@@ -11,6 +11,55 @@ function App(){
   })
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  const inCorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
+
+
+
+
+
+
+
+
+    const addGuessedLetter = useCallback((letter: string)=>{
+      if(guessedLetters.includes(letter)) return
+
+      setGuessedLetters(currentLetters => [...currentLetters, letter])
+    },[guessedLetters ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  useEffect (() => {
+    const handler = (e: KeyboardEvent) =>{
+      const key = e.key
+
+      if(!key.match(/^[a-z]$/)) return
+
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener("keypress",handler)
+
+    return () => {
+      document.removeEventListener("keypress",handler)
+    }
+
+  },[guessedLetters])
+
   return(
     <div style={{
       maxWidth: "800px",
@@ -18,16 +67,26 @@ function App(){
       flexDirection: "column",
       gap: "2rem",
       margin: "0 auto",
-      alignItems: "center"
+      alignItems: "center",
     }}>
 
       <div style={{fontSize: "2rem", textAlign:"center"}}>
         Lose
         Win
       </div>
-      <HangmanDrawing />
-      <HangmanWord />
-      <Keyboard />
+      <HangmanDrawing numberOfGuesses = {inCorrectLetters.length} />
+      <HangmanWord guessedLetters = {guessedLetters} wordToGuess ={wordToGuess} />
+      
+
+
+
+  <div style={{ alignSelf:"stretch" }}>
+
+        <Keyboard
+  
+        />
+      </div>
+      
     </div>
   )
   
--- hangman-vite/src/HangmanDrawing.tsx
+++ hangman-vite/src/HangmanDrawing.tsx
@@ -79,19 +79,18 @@ const LEFT_LEG = (
     }} />
 )
 
+const BODY_PARTS = [HEAD,BODY,RIGHT_ARM,LEFT_ARM,RIGHT_LEG,LEFT_LEG]
+
+type HangmanDrawingProps = {
+    numberOfGuesses: number
+}
 
 
 
+export function HangmanDrawing({ numberOfGuesses } : HangmanDrawingProps){
 
-
-export function HangmanDrawing(){
     return <div style={{ position: "relative" }}>
-        {HEAD}
-        {BODY}
-        {RIGHT_ARM}
-        {LEFT_ARM}
-        {RIGHT_LEG}
-        {LEFT_LEG}
+       {BODY_PARTS.slice(0,numberOfGuesses)}
 
         <div style={{height: "50px", width: "10px", background: "black", position: "absolute", top: 0, right: 0}}/>
         <div style={{height: "10px", width: "200px", background: "black", marginLeft: "120px"}}/>
